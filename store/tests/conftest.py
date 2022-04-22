04db58ce8a082f617403044daf324bae9bd678d1
Efo Coder <efocoder@gmail.com>
2022-04-22 17:09:22 +0000

    tests

---
+++ store/tests/conftest.py
@@ -0,0 +1,16 @@
+from rest_framework.test import APIClient
+import pytest
+from django.contrib.auth.models import User
+
+
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+
+@pytest.fixture
+def authenticate_user(api_client):
+    def do_authenticate_user(is_staff=False):
+        return api_client.force_authenticate(user=User(is_staff=is_staff))
+
+    return do_authenticate_user
